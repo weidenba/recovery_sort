c882be5d076c19c4f4b134cce2c169488ce6e08b
Peter Weidenbach <weidenba@cs.uni-bonn.de>
2017-10-16 17:53:38 +0200

    acceptance tests added

---
+++ test/acceptance/test_main.py
@@ -0,0 +1,24 @@
+from common_helper_process import execute_shell_command_get_return_code
+from common_helper_files import get_dir_of_file
+from tempfile import TemporaryDirectory
+import os
+
+
+SRC_DIR = os.path.join(get_dir_of_file(__file__), '../../')
+TEST_DATA_DIR = os.path.join(get_dir_of_file(__file__), '../data')
+MAINSCRIPT = os.path.join(SRC_DIR, 'recovery_sort.py')
+
+
+def test_help():
+    output, return_code = execute_shell_command_get_return_code('{} -h'.format(MAINSCRIPT), timeout=5)
+    assert return_code == 0
+    assert 'usage: recovery_sort.py' in output
+
+
+def test_no_filter():
+    tmp_dir = TemporaryDirectory()
+    output, return_code = execute_shell_command_get_return_code('{} {} {}'.format(MAINSCRIPT, TEST_DATA_DIR, tmp_dir.name), timeout=10)
+    assert return_code == 0
+    assert '[recovery_sort][INFO]: Re-organizing complete' in output
+    assert '[ReOrganizer][INFO]: 1 files ignored -> duplicate' in output
+    tmp_dir.cleanup()
